c4c31c7f8290244fce59a1d92b6b9fc0bd9eea98
Tvtig <thavendren@gmail.com>
2025-09-19 07:48:44 +0200

    Updated steam url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const STEAM_URL =
-  "https://store.steampowered.com/app/000000/Race_Then_Retry/"; // TODO: replace with your real Steam app URL
+  "https://store.steampowered.com/app/4004940/Race_Then_Retry";
 
 export const SUPPORT_EMAIL = "support@lightheartedtech.com";
